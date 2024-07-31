24292c0f29db142b26a0624d9e3bc3d13e623ca1
AaronAPerez <aaperez06@gmail.com>
2024-07-30 20:08:28 -0700

    update

--- api/Services/UserService.cs
+++ api/Services/UserService.cs
@@ -2,11 +2,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Models.DTO;
+using api.Services.Context;
 
-namespace api.Services
-{
+namespace api.Services;
+
     public class UserService
     {
+        private readonly DataContext _context;
+
+        public UserService(DataContext context)
+        {
+            _context = context;
+        }
+
+        // Helper functions to help us check if the user exist
+        // DoesUserExist
+
+        public bool DoesUserExist(string username)
+        {
+            
+        }
+
+
+        // Adding user logic
+        public bool AddUser(CreateAccountDTO userToAdd)
+        {
+            // If the user already exist
+
+        }
         
     }
-}
